95ef5fc3629077c0476aee162c7e43f757715a44
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-07-17 11:23:52 -0400

    We have a GUI.

--- pygameSubstrate.py
+++ pygameSubstrate.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import PIL.Image
 import PIL.PngImagePlugin
 import pygame
+import pygame_gui
 
 from Substrate import Substrate
 
@@ -13,10 +14,15 @@ import jsons
 
 class PygameSubstrate(Substrate):
     surface: pygame.Surface
+    window_surface: pygame.Surface
+    form_surface: pygame.Surface
+    manager: pygame_gui.UIManager
+    show_form: bool
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
         logging.info("__init__ %s", self)
+        super().__init__(**kwargs)
+        self.show_form = False
 
     def graphics_batch_start(self):
         pass
@@ -35,7 +41,15 @@ class PygameSubstrate(Substrate):
 
     def graphics_initialize(self):
         logging.info("initialize %s %s", self.parameters.width, self.parameters.height)
-        self.surface = pygame.display.set_mode((self.parameters.width, self.parameters.height))
+        self.window_surface = pygame.display.set_mode((self.parameters.width, self.parameters.height))
+        self.surface = pygame.Surface((self.parameters.width, self.parameters.height))
+
+    def update(self):
+        super().update()
+        self.window_surface.blit(self.surface, (0, 0))
+        if self.show_form:
+            self.manager.draw_ui(self.form_surface)
+            self.window_surface.blit(self.form_surface, (0, 0))
 
 
 def save(substrate, substrate_parameters):
@@ -64,15 +78,32 @@ def save(substrate, substrate_parameters):
         f.write(jsons.dumps({"parameters": substrate_parameters, "status": status}, verbose=True))
 
 
+def transparent_surface(size, alpha, color=(0, 0, 0)):
+    surf = pygame.Surface(size, pygame.SRCALPHA)
+    surf.fill((color[0], color[1], color[2], alpha))
+    return surf
+
+
 def run(substrate_parameters=None):
     substrate = PygameSubstrate(parameters=substrate_parameters)
 
     pygame.init()
+
     substrate.update()  # prime the pump
+
+    substrate.form_surface = transparent_surface((640, 480), 0x80, (128, 128, 128))
+    substrate.manager = pygame_gui.UIManager((640, 480))
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                                text='Say Hello',
+                                                manager=substrate.manager)
+
     need_to_exit = False
     paused = False
     was_done = False
+    clock = pygame.time.Clock()
+
     while not need_to_exit:
+        time_delta = clock.tick(60) / 1000.0
         step = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -86,16 +117,19 @@ def run(substrate_parameters=None):
                 keystroke = event.dict.get('text', '').lower()
                 if keystroke == 'p':
                     paused = not paused
-                if keystroke == 'q':
+                elif keystroke == 'q':
                     substrate.quiesced = True
-                if keystroke == 's':
+                elif keystroke == 's':
                     save(substrate, substrate_parameters)
-                if keystroke == ' ':
+                elif keystroke == ' ':
                     step = True
+                elif keystroke == '?':
+                    substrate.show_form = not substrate.show_form
             elif event.type in (pygame.KEYUP, pygame.KEYDOWN):
                 pass
             else:
                 pass
+            substrate.manager.process_events(event)
         if not paused:
             step = True
         if step and not substrate.done:
@@ -106,4 +140,5 @@ def run(substrate_parameters=None):
         if not was_done and substrate.done:
             save(substrate, substrate_parameters)
             was_done = True
+        substrate.manager.update(time_delta)
 
